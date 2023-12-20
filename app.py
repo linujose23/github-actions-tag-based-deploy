59460fa96761ef73167987f24333148f256f9645
LinuXminds <linu@datazoom.com>
2023-12-21 00:28:59 +0530

    text changed

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask('hello-world')
 @app.route('/')
 def hello():
- return "Hello please merge this PR from searce BLR !!!\n"
+ return "Hello please merge this PR from searce BLore !!!\n"
 if __name__ == '__main__':
  app.run(host = '0.0.0.0', port = 8080)
