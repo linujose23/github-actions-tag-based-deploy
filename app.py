c1c7e5d7ed1cba02273360cf0e9a0b9728d5ae1a
LinuXminds <linu@datazoom.com>
2023-12-22 17:08:56 +0530

    added v1 code

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask('hello-world')
 @app.route('/')
 def hello():
- return "Hello this is a release tag based approach !!!\n"
+ return "Hello this is a release tag based approach v1 !!!\n"
 if __name__ == '__main__':
  app.run(host = '0.0.0.0', port = 8080)
